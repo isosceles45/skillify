bad010e8a9b8bbbb3e3c2c098e23effd5ba25edf
isosceles45 <atharvasardal06@gmail.com>
2024-08-25 23:51:45 +0530

    auth-start

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div>
       <h2>Atharva Sardal</h2>
+      <Button variant="outline">Button</Button>
     </div>
   );
 }
